a375dda634f08f248854a077b2016583c61849d7
siphiwo <siphiwojulayi462@gmail>
2021-01-19 14:28:52 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import PokemonThumb from './components/PokemonThumb'
+import PokemonDetails from './components/PokemonDetails'
 
-function App() {
+const App = () => {
+
+   const[allPokemons, setAllPokemons] = useState([])
+   const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
+
+    setLoadMore(data.next)
+
+    function createPokemonObject(results)  {
+      results.forEach( async pokemon => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data =  await res.json()
+        setAllPokemons( currentList => [...currentList, data])
+        await allPokemons.sort((a, b) => a.id - b.id)
+      })
+    }
+    createPokemonObject(data.results)
+  }
+
+ useEffect(() => {
+  getAllPokemons()
+ }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-contaner">
+      <h1>Pokemon Evolution</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+          {allPokemons.map( (pokemonStats, index) => 
+            <PokemonThumb
+              key={index}
+              id={pokemonStats.id}
+              image={pokemonStats.sprites.other.dream_world.front_default}
+              name={pokemonStats.name}
+              type={pokemonStats.types[0].type.name}
+            />)}
+          
+        </div>
+          <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/PokemonThumb.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PokemonThumb = ({id, image, name, type, _callback }) => {
+    const style = type + " thumb-container";
+    return (
+        <div className={style}>
+            <div className="number"><small>#0{id}</small></div>
+            <img src={image} alt={name} />
+            <div className="detail-wrapper">
+                <h3>{name}</h3>
+                <small>Type: {type}</small>
+            </div>
+        </div>
+    )
+}
+
+export default PokemonThumb
